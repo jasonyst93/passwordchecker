24ccf6ef5a9ce377c7d9869e5b3b1720b5458c85
Jason Tam <jasonyst93@gmail.com>
2024-08-22 19:28:34 +0800

    completed

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests 
 import hashlib
+import sys
 
 def request_api_data(query_char):
     url ='https://api.pwnedpasswords.com/range/' + query_char
@@ -11,10 +12,11 @@ def request_api_data(query_char):
 
 def get_password_leak_count(hashes,hash_to_check):
     hashes = (line.split(':')for line in hashes.text.splitlines()) 
+    # if our tail found in tails from the matched list -> return count
     for h ,count in hashes:
-        if h === hash_to_check: # if our tail found in tails from the matched list -> return count
+        if h == hash_to_check: 
             return count 
-        return 0
+    return 0
 
 
 def pwned_api_check(password):    
@@ -25,7 +27,14 @@ def pwned_api_check(password):
 
 # A main function recieve args and print the result
 def main(args):
-    pass
+    for password in args: #loop via all the passwords 
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times in pwned.... Please use another password')
+        else:
+            print(f'{password} is good')
+    return 'done!'
 
-main()
+#python3 checkmypass.py[0] password[1] abcd1234[2] #all the args from 1 
+main(sys.argv[1:]) 
 
